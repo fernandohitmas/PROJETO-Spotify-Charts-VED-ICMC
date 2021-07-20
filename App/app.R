e9b03718b903f19fa6c44dc9d2cbc2ca7fd36994
fernandohitmas <fernando.masumoto@gmail.br>
2021-07-20 15:52:42 -0300

    Organização do aplicativo principal através da adição de comentários e retirada de comandos desnecessários

--- App/app.R
+++ App/app.R
@@ -7,28 +7,23 @@ library(leaflet)
 library(plotly)
 library(data.table)
 
-dados <- readRDS("paises_e_nome_abreviado.rds")
+# Data Import
+data.ranking <- read_csv("../Data/paises_e_nome_abreviado.csv")
+data.total.daily <- read_csv("../Data/all_countries_by_date.csv")
+data.countries.list <- read_csv("../Data/country_names.csv")
+data.map <- world
 
-musicas.mais.tocadas <- dados %>%
+# Total Streams by country - from the available period in the spotify charts website and only for the top 200 musics.
+data.total.country <- data.total.daily %>%
   group_by(Abreviatted) %>%
   summarise(total_plays = sum(Total.Streams))
 
-artistas.mais.tocadas <- dados %>%
-  group_by(Artist) %>%
-  summarise(total_plays = sum(Total.Streams))
+# Concatenation of Spotify Data to World data by utilizing inner_join
+data.map <- inner_join(data.map, data.total.country, by = c("iso_a2" = "Abreviatted"))
+# Adding the Streams per Population for each country.
+data.map <- data.map %>% mutate(streams_per_pop = total_plays/pop)
 
-mapa <- world
 
-dados_mapa <- inner_join(mapa, musicas.mais.tocadas, by = c("iso_a2" = "Abreviatted"))
-
-dados_mapa <- dados_mapa %>% mutate(streams_por_pop = total_plays/pop)
-
-todos.paises <- read_csv("all_countries_by_date.csv")
-
-countries.spotify <- read_csv("country_names.csv")
-
-lista.paises <- countries.spotify %>%
-  filter(Name != "Global" & Name != "Hong Kong" & Name != "Singapore" & Name != "Cyprus")
 ui <- fluidPage(
   titlePanel("Visualização de dados do Spotify Charts"),
   
@@ -44,18 +39,19 @@ ui <- fluidPage(
               )
     )
   )
-  
 )   
 
 
 server <- function(input, output, session) {
   
+  # Map Output
   output$mapa <- renderLeaflet({
     
-    colorData <- dados_mapa$streams_por_pop
+    # Interval in which the color palette (pal = RdPu) will be applied 
+    colorData <- data.map$streams_per_pop
     pal <- colorBin("RdPu", colorData, na.color = "#808080")
     
-    dados_mapa %>%
+    data.map %>%
       leaflet %>%
       addTiles() %>%
       addPolygons(fillColor = ~pal(colorData), label = ~name_long,
@@ -74,9 +70,9 @@ server <- function(input, output, session) {
                   popup = ~ paste0(
                     sep = " ",
                     "<b>", name_long, "</b><br>",
-                    "Total Streams: ", round(total_plays/1000000), "M<br>",
-                    "Population: ", round(pop/1000000,2) , "M<br>",
-                    "Streams/Population: ", round(streams_por_pop,2), "<br>"
+                    "Total de Streams: ", round(total_plays/1e+9,2), "B<br>",
+                    "População: ", round(pop/1000000,2) , "M<br>",
+                    "Streams/População: ", round(streams_per_pop,2), "<br>"
                   )) %>%
       addLegend("topright",
                 title = "Streams per<br> population",
@@ -85,18 +81,24 @@ server <- function(input, output, session) {
     
     
   })
-  
+   
+  # Table Output
   output$tabela <- renderPlotly({
-    pais <- input$mapa_shape_click
-    if(is.null(pais)) return()
     
-    rank <- dados %>%
-      filter(Abreviatted == pais$id) %>%
+    # Leaflet Input via map click
+    p <- input$mapa_shape_click
+    country <- p$id
+    
+    # If the user has not clicked country it won't display any Table
+    if(is.null(country)){return()}  
+    
+    # Ranking Data filtered by the selected country 
+    rank <- data.ranking %>%
+      filter(Abreviatted == country) %>%
       select(Artist, Song,Total.Streams) %>%
       t()
     
-    #browser()
-    dados %>% filter(Abreviatted == pais$id) %>%
+    dados %>% filter(Abreviatted == country) %>%
       plot_ly(
         type = 'table',
         columnwidth = c(200, 200, 200),
@@ -118,25 +120,32 @@ server <- function(input, output, session) {
       )
   })
   
+  # Time Series output
   output$serie_temp <- renderPlotly({
     
-    comeco <- input$data[1]
-    fim <- input$data[2]
-    
+    # Leaflet Input via map click
     p <- input$mapa_shape_click
+    country <- p$id
     
-    pais <- p$id
+    # If the user has not clicked country it won't display any Time Series
+    if(is.null(country)){return()}  
     
-    pais.abrev <- lista.paises$Abreviatted[lista.paises$Abreviatted %in% pais]
-    pais.completo <- lista.paises$Name[lista.paises$Abreviatted %in% pais]
-    fit <- lm(Total.Streams ~ Date, data = todos.paises%>% filter(Abreviatted %in% pais.abrev) %>%
+    # Country identification 
+    country.abrev <- data.countries.list$Abreviatted[data.countries.list$Abreviatted %in% country]
+    country.complete <- data.countries.list$Name[data.countries.list$Abreviatted %in% country]
+    
+    # Linear Regression Model for selected country
+    fit <- lm(Total.Streams ~ Date, data = data.total.daily%>% filter(Abreviatted %in% country.abrev) %>%
                 group_by(Abreviatted))
-    todos.paises %>%
-      filter(Abreviatted %in% pais.abrev) %>%
-      group_by(Abreviatted) %>%
+    
+    data.total.daily %>%
+      filter(Abreviatted %in% country.abrev) %>%
       plot_ly(type="scatter",mode="line")%>%
       add_lines(x=~Date,y=~Total.Streams, name="Série Temporal")%>%
-      layout(title = list(text = ~pais.completo, x = 0.55, y = 0.92), legend = list(y=.95, x=.05))%>%                                 
+      layout(title = list(text = ~country.complete, x = 0.55, y = 0.92), 
+             legend = list(y=.95, x=.05), 
+             xaxis = list(title="Data"), 
+             yaxis = list(title="Total de Streams"))%>%                                 
       add_lines(x = ~Date, y = fitted(fit), name="Regressão Linear")%>%
       add_lines(x=~Date,y=~frollmean(Total.Streams,7),name="Média Semanal")  
   })
